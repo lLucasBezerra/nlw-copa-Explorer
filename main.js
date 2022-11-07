730f50b0dacae5aed2464e1b6d18b3bc5a8bb755
José Lucas <bezerra00714@hotmail.com>
2022-11-07 10:30:34 -0300

    adicionando animções e projeto finalizado

--- main.js
+++ main.js
@@ -20,61 +20,35 @@ function createCard(date, day, games){
 }
 
 
-document.querySelector("#app").innerHTML = `
-    <header>
-        <img src="./assets/logo.svg" alt="logo da nlw">
-    </header>
+document.querySelector("#cards").innerHTML = 
 
-    <main id="cards">
-        <div class="card">
-            <h2>24/11 <span>Quinta</span></h2>
-
-            <ul>
-                <li>
-                    <img src="./assets/Bandeiras dos países/icon=swaziland.svg" alt="bandeira da swaziland">
-                    <strong>7:00</strong>
-                    <img src="./assets/Bandeiras dos países/icon=cameroon.svg" alt="bandeira dos camarões">
-                </li>
-                <li>
-                    <img src="./assets/Bandeiras dos países/icon=uruguay.svg" alt="bandeira do uruguai">
-                    <strong>10:00</strong>
-                    <img src="./assets/Bandeiras dos países/icon=south korea.svg" alt="bandeira da coreia do sul">
-                </li>
-                <li>
-                    <img src="./assets/Bandeiras dos países/icon=portugal.svg" alt="bandeira do portugal">
-                    <strong>13:00</strong>
-                    <img src="./assets/Bandeiras dos países/icon=ghana.svg" alt="bandeira da gana">
-                </li>
-                <li>
-                    <img src="./assets/Bandeiras dos países/icon=brazil.svg" alt="bandeira do brasil">
-                    <strong>16:00</strong>
-                    <img src="./assets/Bandeiras dos países/icon=serbia.svg" alt="bndeira da servia">
-                </li>
-            </ul>
-
-        </div>
-
+    
+        createCard("24/11", "quinta", 
+        createGame("switzerland", "cameroon", "07:00") +
+        createGame("uruguay", "south korea", "10:00") +
+        createGame("portugal", "ghana", "13:00") +
+        createGame("brazil", "serbia", "16:00") 
+        ) +
       
-        ${createCard("28/11", "segunda", 
+        createCard("28/11", "segunda", 
         createGame("cameroon", "serbia", "07:00") +
         createGame("south korea", "ghana", "10:00") +
         createGame("brazil", "switzerland", "13:00") +
         createGame("portugal", "uruguay", "16:00") 
-        )}
+        ) +
        
-        ${createCard("30/11", "quarta", 
+        createCard("30/11", "quarta", 
         createGame("tunisia", "france", "12:00") +
         createGame("australia", "denmark", "12:00") +
         createGame("poland", "argentina", "16:00") +
         createGame("saudi arabia", "mexico", "16:00")
-        )}
+        ) +
        
-        ${createCard("02/12", "sexta", 
+        createCard("02/12", "sexta", 
         createGame("south korea", "portugal", "12:00") +
         createGame("ghana", "uruguay", "12:00") +
         createGame("serbia", "switzerland", "16:00") +
         createGame("cameroon", "brazil", "16:00")
-        )}
+        )
 
-    </main>
-`;
+;
